a8f2ba151efb27a2000dc46058410a2a02bef4b7
antilopinae <mikbas31@gmail.com>
2024-08-12 12:51:04 +0200

    Finally

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-rm -R build
+#rm -R build
 #cmake -G "Ninja" -D CMAKE_C_COMPILER="usr/bin/gcc" -D CMAKE_CXX_COMPILER="/usr/bin/clang-18" -B build -S .
-cmake -DLLVM_ENABLE_PROJECTS=clang -DCMAKE_BUILD_TYPE=Release -D CMAKE_C_COMPILER="/usr/bin/clang" -D CMAKE_CXX_COMPILER="/usr/bin/clang++" -G Ninja -B build -S .  # note, the ninja build tool is faster than the make tool referenced in the docs.
+cmake -DLLVM_ENABLE_PROJECTS=clang -DCMAKE_BUILD_TYPE=Debug -D CMAKE_C_COMPILER="/usr/bin/clang" -D CMAKE_CXX_COMPILER="/usr/bin/clang++" -G Ninja -B build -S .  # note, the ninja build tool is faster than the make tool referenced in the docs.
 cmake --build build
 ./build/bin/bootstrap
