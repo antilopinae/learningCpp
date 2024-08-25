f985a058bd73e8adf9e1359935bd84c74516016c
antilopinae <mikbas31@gmail.com>
2024-08-25 15:00:28 +0200

    allr ok

--- modules/modules.cpp
+++ modules/modules.cpp
@@ -2,12 +2,25 @@ module;
 
 #include <iostream>
 
+//module partition
+export module A:B; // declares a module interface unit for module A, partition :B
+//A-B.cpp
+
+module A:C; //A-C.cpp
+
+export module A;
+
+import :C;
+export import :B;
+
 export module modules;
 
 export void test_modules()
 {
     std::cout << "Test modules" << std::endl;
 }
+
+
 
 export int test_modules_f();
 
